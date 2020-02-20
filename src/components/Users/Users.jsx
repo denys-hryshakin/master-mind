775f4b7b1deb7ba26c0eab29d9ba585957e30a95
Dennis <32460055+sk1fer@users.noreply.github.com>
2020-02-20 17:21:26 +0200

    Changes

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,55 +1,26 @@
 import React from 'react';
 import styles from './Users.module.css';
+import * as axios from 'axios';
+import avatar from './../../assets/images/avatar2.jpg'
 
-let Users = (props) => {
-    if (props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl: 'http://localhost:3000/static/media/avatar2.d86f9d68.jpg',
-                followed: true,
-                fullname: 'Andrey',
-                time: 'Last seen at 13:50',
-                status: 'Status #1',
-                location: { city: 'Odessa', country: 'Ukraine' }
-            },
-            {
-                id: 2,
-                photoUrl: 'http://localhost:3000/static/media/avatar2.d86f9d68.jpg',
-                followed: false,
-                fullname: 'Genadiy',
-                time: 'Last seen at 09:25',
-                status: 'Status #2',
-                location: { city: 'Minsk', country: 'Belarus' }
-            },
-            {
-                id: 3,
-                photoUrl: 'http://localhost:3000/static/media/avatar2.d86f9d68.jpg',
-                followed: true,
-                fullname: 'Denis',
-                time: 'Last seen at 08:12',
-                status: 'Status #3',
-                location: { city: 'Kiev', country: 'Ukraine' }
-            },
-            {
-                id: 4,
-                photoUrl: 'http://localhost:3000/static/media/avatar2.d86f9d68.jpg',
-                followed: false,
-                fullname: 'Dima',
-                time: 'Last seen at 12:55',
-                status: 'Status #4',
-                location: { city: 'Lviv', country: 'Ukraine' }
-            }
-        ])
+let Users = props => {
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    props.setUsers(response.data.items);
+                });
+        }
     }
     return (
         <div className={styles.item}>
+        <button onClick={getUsers}>Get Users</button>
             {
                 props.users.map(u => <div key={u.id}>
                     <div className={styles.mediaBlock}>
                         <div>
                             <div className={styles.userImg}>
-                                <img className={styles.userPhoto} src={u.photoUrl} alt="" />
+                                <img className={styles.userPhoto} src={u.photos.small != null ? u.photos.small : avatar} alt="" />
                             </div>
                         </div>
                         <div>
@@ -60,12 +31,12 @@ let Users = (props) => {
                     </div>
                     <div className={styles.userInfoBlock}>
                         <div>
-                            <div>{u.fullname}</div>
+                            <div>{u.name}</div>
                             <div>{u.status}</div>
                         </div>
                         <div>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>{"u.location.country"}</div>
+                            <div>{"u.location.city"}</div>
                         </div>
                     </div>
                 </div>)
