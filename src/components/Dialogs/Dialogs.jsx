81542648a609344454f3784ef3eef8ab6e009f3e
Dennis <32460055+sk1fer@users.noreply.github.com>
2020-04-15 14:27:48 +0300

    1

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -9,7 +9,7 @@ const Dialogs = (props) => {
     let dialogsElements = state.dialogs.map(d => <DialogItem name={d.name} key={d.id} id={d.id} time={d.time} />);
     let messagesElements = state.messages.map(m => <Message message={m.message} key={m.id} />);
     let newMessageBody = state.newMessageBody;
-
+1
     let onSendMessageClick = () => {
         props.sendMessage();
     }
